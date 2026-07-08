582868a1d79351df66a52c529bbee65c9df9ba10
Baladasi Rekha <github_rekharoyal282@gmail.com>
2026-07-08 11:31:32 +0530

    Connect frontend to deployed backend

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://e-commerce-website-3-idg3.onrender.com/api",
 });
 
 export default API;
